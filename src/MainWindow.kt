252a9f0122c030dff7661248f3f1fb15910528ff
Evgeniy Yushin <evgeniy.yushin@outlook.com>
2019-10-03 00:14:34 +0600

    Zero progress...

--- src/MainWindow.kt
+++ src/MainWindow.kt
@@ -1,63 +1,92 @@
 package tictactoe
 
-
+import java.awt.Color
 import java.awt.TextField
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
+import java.util.*
 import javax.swing.*
 
 
-class StartPage : JFrame() {
+class StartPage : JFrame(), ActionListener{
     var size = 3
     val jLabel = JLabel()
     val jButton = JButton("Ок.")
     val jSlider = JSlider()
-    val field: Array<Array<JButton>> = arrayOf()
+    var selection = true
+    val jLabel2 = JLabel("Ход игрока ${if (selection == true) "X" else "O"}")
+    val field = LinkedList<JButton>()
 
     fun createUI() {
-        this.setSize(800, 900)
+        this.setSize(416, 459)
 
-        jLabel.setBounds(195, 180, 100, 18)
+        jLabel.setBounds(168, 180, 100, 18)
         jLabel.text ="Размер поля: "
         this.add(jLabel, null)
 
-        jButton.setBounds(210, 300, 70, 30)
+        jButton.setBounds(173, 300, 70, 30)
         this.add(jButton, null)
 
         jSlider.minimum = 3
         jSlider.maximum = 10
-        jSlider.setBounds(0, 200, 485, 50)
+        jSlider.setBounds(0, 200, 400, 50)
         jSlider.majorTickSpacing = 1
         jSlider.paintTicks = true
         jSlider.snapToTicks = true
         jSlider.paintLabels = true
         this.add(jSlider, null)
 
-        this.contentPane.setLayout(null)
+        this.contentPane.layout = null
         //this.add(getJTextField(), null)
         this.title = "Крестики Нолики"
 
-        val al = BActionListener()
-        jButton.addActionListener(al)
+        jButton.addActionListener(this)
     }
 
-    inner class BActionListener : ActionListener {
-        override fun actionPerformed(e: ActionEvent?) {
-            size = jSlider.value
-            remove(jSlider)
-            remove(jButton)
-            jLabel.setBounds(350, 18, 100, 18)
-            jLabel.text = "Размер поля = $size"
+    fun createField() {
+        remove(jSlider)
+        remove(jButton)
+        for (i in 0 until size) {
+            for (j in 0 until size) {
+                field.add(i + j, JButton())
+                field[i + j].setBounds(j*40, i* 40 + 20, 40, 40)
+                add(field[i + j])
+                field[i + j].isVisible = true
+                field[i + j].addActionListener(this)
+            }
+        }
+        repaint()
 
-            for (i in 0 until size) {
-                for (j in 0 until size) {
-                    field[i][j] = JButton("*")
-                    field[i][j].setBounds(j*40, i* 40, 40, 40)
-                    add(field[i][j])
-                    field[i][j].isVisible = true
+        jLabel.setBounds(0, 0, 110, 21)
+        jLabel.text = "Размер поля = $size"
+        jLabel2.setBounds(324,0,110,21)
+        add(jLabel2)
+        jLabel2.isVisible = true
+    }
+
+    override fun actionPerformed(e: ActionEvent?) {
+        if (e!!.source == jButton) size = jSlider.value
+        
+        for (i in 0 until size) {
+            for(j in 0 until size) {
+                if (e!!.source == field[i + j]) {
+                    if (e.source == field[i + j])
+                    field[i + j].text = if (selection) "X" else "O"
+                    selection != selection
+                    if (selection) field[i + j].background = Color.GREEN else field[i + j].background = Color.RED
                 }
             }
-            repaint()
         }
+        repaint()
     }
+
+   /* inner class FieldButtonActionListener : ActionListener {
+        override fun actionPerformed(e: ActionEvent?) {
+            finalfield[i].addActionListener(finalgameal[i]).run {
+                finalfield[i].text = if (selection) "X" else "O"
+                selection = !selection
+                repaint()
+            }
+        }
+    }*/
 }
--- src/main.kt
+++ src/main.kt
@@ -3,5 +3,6 @@ package tictactoe
 fun main() {
     val UI = StartPage()
     UI.createUI()
+    UI.createField()
     UI.isVisible = true
 }
