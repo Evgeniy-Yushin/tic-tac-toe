e729a812827e367ae70720a4de6fe419aa88e1db
Evgeniy Yushin <evgeniy.yushin@outlook.com>
2019-10-06 12:05:51 +0600

    Adding game rules in Graphic version

--- src/MainWindow.kt
+++ src/MainWindow.kt
@@ -14,11 +14,11 @@ class StartPage : JFrame(), ActionListener{
     val jButton = JButton("Ок.")
     val jSlider = JSlider()
     var selection = true
-    val jLabel2 = JLabel("Ход игрока ${if (selection == true) "X" else "O"}")
+    val jLabel2 = JLabel("Ход игрока ${if (selection) "X" else "O"}")
     val field = Array(10) {Array(10) {JButton(ImageIcon("startbutton.png"))} }
 
     fun createUI() {
-        this.setSize(416, 459)
+        this.setSize(516, 559)
 
         jLabel.setBounds(168, 180, 100, 18)
         jLabel.text ="Размер поля: "
@@ -48,19 +48,20 @@ class StartPage : JFrame(), ActionListener{
     override fun actionPerformed(e: ActionEvent?) {
         if (e!!.source == jButton) {
             size = jSlider.value
-            remove(jLabel)
+            jLabel.text = "Размер поля: ${size}x${size}"
+            jLabel.setBounds(0, 0, 200, 20)
             remove(jSlider)
             remove(jButton)
 
             for (i in 0 until size) {
                 for (j in 0 until size) {
-                    field[i][j].setBounds(40 * j, 40 * i + 20, 40, 40)
+                    field[i][j].setBounds(50 * j, 50 * i + 20, 50, 50)
                     add(field[i][j])
                     field[i][j].isVisible = true
                     field[i][j].addActionListener(this)
                 }
             }
-            jLabel2.setBounds(300, 0, 116, 20)
+            jLabel2.setBounds(423, 0, 116, 20)
             jLabel2.isVisible = true
             add(jLabel2)
             repaint()
@@ -72,11 +73,64 @@ class StartPage : JFrame(), ActionListener{
                 if (e.source == field[i][j]) {
                     field[i][j].text = if (selection) "X" else "O"
                     if (selection) field[i][j].background = Color.GREEN else field[i][j].background = Color.RED
+                    field[i][j].isEnabled = false
                     selection = !selection
+                    jLabel2.text = "Ход игрока ${if (selection) "X" else "O"}"
+
+                    if (endgame()) this.isEnabled = false
                 }
             }
         }
         repaint()
+    }
+
+    fun draw() : Boolean {
+        for (i in 0 until size) for (j in 0 until size) if (field[i][j].isEnabled) return false
+        return true
+    }
+
+    fun winCheck(size: Int) : Boolean {
+        val check: Array<Int> = Array(4) {0;0;0;0}
+
+        for (i in 0 until size) {
+            for (j in 0 until size) {
+                if (field[i][j].background == (if (selection) Color.RED else Color.GREEN)) {
+                    check[0] += 1
+                }
+            }
+            if (check[0] == size) return true else check[0] = 0
+        }
+
+        for (i in 0 until size) {
+            for (j in 0 until size) {
+                if (field[j][i] == (if (selection) Color.RED else Color.GREEN)) {
+                    check[1] += 1
+                }
+            }
+            if (check[1] == size) return true else check[1] = 0
+        }
+
+        for (i in 0 until size) {
+            if (field[i][i] == (if (selection) Color.RED else Color.GREEN)) check[2] += 1
+        }
+
+        for (i in 0 until size) {
+            if (field[i][size - 1 - i] == (if (selection) Color.RED else Color.GREEN)) check[3] += 1
+        }
+        if (check[0] == size || check[1] == size || check[2] == size || check[3] == size) return true
+        return false
+    }
+
+    fun endgame() : Boolean {
+        if (winCheck(size)) {
+            jLabel.text = ("Игрок ${if (selection) 'O' else 'X'} победил. Игра окончена.")
+            return true
+        }
+        if (draw()) {
+            jLabel.text = "Ничья. Игра окончена."
+            return true
+        }
+        else return false
     }
 
    /* inner class FieldButtonActionListener : ActionListener {
