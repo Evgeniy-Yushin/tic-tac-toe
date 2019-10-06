6c13f913c57a30ed36e36c60336bbe8d7f8aa8c6
Evgeniy Yushin <evgeniy.yushin@outlook.com>
2019-10-06 21:40:10 +0600

    Delete usless Yushin'sSliderUI.kt

--- src/tictactoe/Yushin'sSliderUI.kt
+++
@@ -1,9 +0,0 @@
-package tictactoe
-
-import javax.swing.ImageIcon
-import javax.swing.JButton
-import javax.swing.JSlider
-import javax.swing.plaf.basic.BasicButtonUI
-
-class YushinButtonUI : BasicButtonUI() {
-}
